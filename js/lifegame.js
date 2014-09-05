93ef7286f367c43be17408afdf6865fa19a696e2
koturn <jeak.koutan.apple@gmail.com>
2014-09-06 00:29:37 +0900

    Refactoring JS codes

--- js/lifegame.js
+++ js/lifegame.js
@@ -1,66 +1,70 @@
 'use strict';
 
 function LifegameBoard(canvasId, cellSize, cellColor) {
-  this.canvas = document.getElementById(canvasId);
-  this.ctx = this.canvas.getContext('2d');
-  this.ctx.fillStyle = cellColor;
-  this.width = Math.floor(this.canvas.width / cellSize);
-  this.height = Math.floor(this.canvas.height / cellSize);
-  this.cellSize = cellSize;
-  this.board = new Array(this.height * this.width);
+  this._canvas = document.getElementById(canvasId);
+  this._ctx = this._canvas.getContext('2d');
+  this._ctx.fillStyle = cellColor;
+  this._nCol = Math.floor(this._canvas.width / cellSize);
+  this._nRow = Math.floor(this._canvas.height / cellSize);
+  this._cellSize = cellSize;
+  this._board = new Array(this._nRow * this._nCol);
+};
 
-  this.initBoard = function() {
-    var board = this.board;
-    for (var i = 1; i < this.height - 1; i++) {
-      for (var j = 1; j < this.width - 1; j++) {
-        board[i * this.width + j] = (Math.random() < 0.3 ? 1 : 0);
+
+LifegameBoard.prototype.initBoard = function(genRate) {
+  genRate = genRate || 0.5;
+  var board = this._board;
+  for (var i = 1; i < this._nRow - 1; i++) {
+    for (var j = 1; j < this._nCol - 1; j++) {
+      board[i * this._nCol + j] = (Math.random() < genRate ? 1 : 0);
+    }
+  }
+  for (var i = 0; i < this._nRow; i++) {
+    board[i * this._nCol] = 0;
+    board[i * this._nCol + (this._nCol - 1)] = 0;
+  }
+  var offset = (this._nRow - 1) * this._nCol;
+  for (var i = 0; i < this._nCol; i++) {
+    board[i] = 0;
+    board[offset + i] = 0;
+  }
+};
+
+
+LifegameBoard.prototype.update = function() {
+  var board = this._board.slice();
+  for (var i = 1; i < this._nRow - 1; i++) {
+    for (var j = 1; j < this._nCol - 1; j++) {
+      var sum = this.countAliveCells(this._board, j, i);
+      if (sum == 3) {
+        board[i * this._nCol + j] = 1;
+      } else if (sum != 2) {
+        board[i * this._nCol + j] = 0;
       }
     }
-    for (var i = 0; i < this.height; i++) {
-      board[i * this.width] = 0;
-      board[i * this.width + (this.width - 1)] = 0;
-    }
-    var offset = (this.height - 1) * this.width;
-    for (var i = 0; i < this.width; i++) {
-      board[i] = 0;
-      board[offset + i] = 0;
-    }
-  };
-  this.initBoard();
+  }
+  this._board = board;
+};
 
-  this.update = function() {
-    var board = this.board.slice();
-    for (var i = 1; i < this.height - 1; i++) {
-      for (var j = 1; j < this.width - 1; j++) {
-        var sum = this.countAliveCells(this.board, j, i);
-        if (sum == 3) {
-          board[i * this.width + j] = 1;
-        } else if (sum != 2) {
-          board[i * this.width + j] = 0;
-        }
+
+LifegameBoard.prototype.draw = function() {
+  this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+  var board = this._board;
+  for (var i = 1; i < this._nRow - 1; i++) {
+    for (var j = 1; j < this._nCol - 1; j++) {
+      if (board[i * this._nCol + j] == 1) {
+        this._ctx.fillRect(j * this._cellSize, i * this._cellSize, this._cellSize, this._cellSize);
       }
     }
-    this.board = board;
-  };
+  }
+};
 
-  this.draw = function() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    var board = this.board;
-    for (var i = 1; i < this.height - 1; i++) {
-      for (var j = 1; j < this.width - 1; j++) {
-        if (board[i * this.width + j] == 1) {
-          this.ctx.fillRect(i * this.cellSize, j * this.cellSize, this.cellSize, this.cellSize);
-        }
-      }
-    }
-  };
 
-  this.countAliveCells = function(board, x, y) {
-    var yOffset1 = (y - 1) * this.width;
-    var yOffset2 = y * this.width;
-    var yOffset3 = (y + 1) * this.width;
-    return board[yOffset1 + x - 1] + board[yOffset1 + x] + board[yOffset1 + x + 1] +
-      board[yOffset2 + x - 1] + board[yOffset2 + x + 1] +
-      board[yOffset3 + x - 1] + board[yOffset3 + x] + board[yOffset3 + x + 1];
-  };
-}
+LifegameBoard.prototype.countAliveCells = function(board, x, y) {
+  var yOffset1 = (y - 1) * this._nCol;
+  var yOffset2 = y * this._nCol;
+  var yOffset3 = (y + 1) * this._nCol;
+  return board[yOffset1 + x - 1] + board[yOffset1 + x] + board[yOffset1 + x + 1] +
+    board[yOffset2 + x - 1] + board[yOffset2 + x + 1] +
+    board[yOffset3 + x - 1] + board[yOffset3 + x] + board[yOffset3 + x + 1];
+};
